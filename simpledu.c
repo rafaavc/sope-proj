adb04d0f163266de57bc33c9b723c1ec1afa9c32
Xavier-Pisco <x.pisco@gmail.com>
2020-03-31 12:27:44 +0100

    Working with -a, --max_depth, but size of folder doesn't include files nor subfolders

--- simpledu.c
+++ simpledu.c
@@ -12,11 +12,40 @@
 #include <math.h>
 
 
-#define MAX_PATH_SIZE 512
+#define MAX_STRING_SIZE 512
 #define MAX_DEPTH 'm'
 
 
 int block_size = 1024, max_depth = __INT_MAX__;
+bool all = false, bytes = false, count_links = false, dereference = false, separate_dirs = false;
+
+bool check_stat(struct stat stat_buf){
+    if (S_ISLNK(stat_buf.st_mode)){
+        if (all || dereference) return true;
+    } 
+    else if(S_ISREG(stat_buf.st_mode)){
+        if (all) return true;
+    }
+    return false;
+}
+
+void prepare_command(char **cmd, char* path){
+    char * tmp = malloc(MAX_STRING_SIZE);
+    int i = 0;
+    cmd[i++] = "simpledu";
+    if (all) cmd[i++] = "-a";
+    if (bytes) cmd[i++] = "-b";
+    sprintf(tmp, "-B=%d", block_size);
+    cmd[i++] = tmp;
+    if (count_links) cmd[i++] = "-l";
+    if (dereference) cmd[i++] = "-L";
+    if (separate_dirs) cmd[i++] = "-s";
+    sprintf(tmp, "--max-depth=%d", max_depth - 1);
+    cmd[i++] = tmp;
+    cmd[i++] = path;
+    cmd[i] = NULL;
+}
+
 
 //struct para opções longas funcionarem com getopt_long
 struct option const long_options[] = {
@@ -33,8 +62,9 @@ struct option const long_options[] = {
 int main(int argc, char* argv[]){
 
     char c;
+    char *path = malloc(MAX_STRING_SIZE);
+    strcpy(path,".");
     //bool para opções variadas
-    bool all = false, bytes = false, count_links = false, dereference = false, separate_dirs = false;
 
 
     //lê todas as opções (-char) que forem passadas no argv
@@ -71,36 +101,63 @@ int main(int argc, char* argv[]){
                     exit(2);
                 }
                 break;
+            default:
+                break;
         }
 
+    }
+
+    int i = 1;
+    while(i < argc && argv[i] != NULL){
+        if (argv[i][0] != '-'){
+            path = argv[i];
+            break;
+        }
+        i++;
     }
 
     DIR *dirp;
     struct dirent *direntp;
     struct stat stat_buf;
-    char *str;
+    char *newpath = malloc(MAX_STRING_SIZE);
 
-    if ((dirp = opendir(".")) == NULL)
+    if ((dirp = opendir(path)) == NULL)
     {
-        perror(".");
+        perror(path);
         exit(2);
     }
     while ((direntp = readdir( dirp)) != NULL)
     {
-        if (lstat(direntp->d_name, &stat_buf) != 0) {
-            perror(".");
+        sprintf(newpath, "%s/%s", path, direntp->d_name);
+
+        if (lstat(newpath, &stat_buf) != 0) {
+            perror(path);
             exit(3);
         }
         if (strcmp(direntp->d_name, "..") == 0) continue; 
-        if (S_ISREG(stat_buf.st_mode) && all){
+
+        if (check_stat(stat_buf)){
+            // Tamanho do bloco = 512, para ter tamanho em 4096, nº blocos/8
             int file_space = stat_buf.st_blksize * stat_buf.st_blocks/8;
-            printf("%-7d ./%s\n", (int)ceil(file_space/block_size), direntp->d_name);
+            printf("%-7d %s\n", (int)ceil(file_space/block_size), newpath);
+
         } else if (S_ISDIR(stat_buf.st_mode)){
-            int file_space = stat_buf.st_blksize * stat_buf.st_blocks/8;
-            if (strcmp(direntp->d_name,".") == 0)
-                printf("%-7d %s\n", (int)ceil(file_space/block_size), direntp->d_name);
-            else 
-                printf("%-7d ./%s\n", (int)ceil(file_space/block_size), direntp->d_name);
+            if(strcmp(direntp->d_name, ".") != 0){
+                if (fork() > 0){
+                    int file_space = stat_buf.st_blksize * stat_buf.st_blocks/8;
+                    printf("%-7d %s\n", (int)ceil(file_space/block_size), newpath);
+                    wait(NULL);
+                } else if (max_depth > 1){
+                    char ** command;
+                    prepare_command(command, newpath);
+                    closedir(dirp);
+                    execv(command[0], command);
+                } else {
+                    exit(0);
+                }
+            }
+
+
         }
     }
     closedir(dirp);
