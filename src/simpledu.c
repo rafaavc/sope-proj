896975c9d0a20b0f0e3e8460e24aea68bb0437b3
Rafael Cristino <rafavc2508@gmail.com>
2020-04-08 22:01:15 +0100

    small improvements

--- src/simpledu.c
+++ src/simpledu.c
@@ -259,14 +259,6 @@ void checkDirectory(bool masterProcess, char * path, int currentDepth, int outpu
             terminateProcess(EXIT_FAILURE);
         }
 
-        /* struct dirent {
-            ino_t          d_ino;        Inode number 
-            off_t          d_off;        Not an offset; see below 
-            unsigned short d_reclen;     Length of this record 
-            unsigned char  d_type;       Type of file; not supported by all filesystem types 
-            char           d_name[256];  Null-terminated filename 
-        }; */
-
         while ((direntp = readdir( dirp)) != NULL)
         {
             fileSize = 0;
@@ -280,22 +272,6 @@ void checkDirectory(bool masterProcess, char * path, int currentDepth, int outpu
                 sprintf(newpath, "%s/%s", path, direntp->d_name);
             }
 
-            /* struct stat {
-                dev_t     st_dev;      ID of device containing file 
-                ino_t     st_ino;      inode number 
-                mode_t    st_mode;     protection 
-                nlink_t   st_nlink;    number of hard links 
-                uid_t     st_uid;      user ID of owner 
-                gid_t     st_gid;      group ID of owner 
-                dev_t     st_rdev;     device ID (if special file) 
-                off_t     st_size;     total size, in bytes 
-                blksize_t st_blksize;  blocksize for file system I/O 
-                blkcnt_t  st_blocks;   number of 512B blocks allocated 
-                time_t    st_atime;    time of last access 
-                time_t    st_mtime;    time of last modification 
-                time_t    st_ctime;    time of last status change 
-            };
-            returns information about the file in the struct stat format */
             if (lstat(newpath, &stat_buf) != 0) {
                 perror(newpath);
                 terminateProcess(EXIT_FAILURE);
@@ -359,17 +335,14 @@ void checkDirectory(bool masterProcess, char * path, int currentDepth, int outpu
         }
     }
 
+    sprintf(info, "%d", currentDirSize);
+    writePipe(outputFD, info, sizeof(info));
 
-    char *b = malloc(MAX_STRING_SIZE);
-    sprintf(b, "%d", currentDirSize);
-    writePipe(outputFD, b, sizeof(b));
-    free(b);
     free(info);
 
     closedir(dirp);
 }
 
-// TODO check return codes
 int main(int argc, char* argv[]){ 
     setLogFilename();
     openLogFile();
